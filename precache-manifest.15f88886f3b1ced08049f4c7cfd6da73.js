f7e1e0b7624ac71b195522cff6af38604cb5ff98
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-08-30 01:23:03 +0300

    Updates

---
+++ precache-manifest.15f88886f3b1ced08049f4c7cfd6da73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfe48749b76d0c666d76b857f2260d2",
+    "url": "/the-gallery/index.html"
+  },
+  {
+    "revision": "0904ea15a72f866b25ab",
+    "url": "/the-gallery/static/css/main.84c1d1a4.chunk.css"
+  },
+  {
+    "revision": "00a8b0ba331eca165915",
+    "url": "/the-gallery/static/js/2.e96036e8.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/the-gallery/static/js/2.e96036e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0904ea15a72f866b25ab",
+    "url": "/the-gallery/static/js/main.b1a89451.chunk.js"
+  },
+  {
+    "revision": "c21e0bb5080bc24f1966",
+    "url": "/the-gallery/static/js/runtime-main.1a3415d9.js"
+  }
+]);
